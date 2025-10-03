35703151800187fd735bc238dc4927ffd687e7b9
jitendra Khadka <jitendrakhadka4@gmail.com>
2025-10-03 12:54:21 +0900

    WIP: remove unwanted tracked files

--- backend/cashbook_backend/settings.py
+++ backend/cashbook_backend/settings.py
@@ -183,7 +183,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://192.168.0.86:3000"
+    "http://192.168.0.86:3000",
+    'http://0.0.0.0:8080',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
